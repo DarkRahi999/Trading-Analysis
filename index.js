51f887b9f77148b41d65ddb42e76c688dab195b1
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-01-05 18:58:26 +0600

    some condition added but not finish yet

--- index.js
+++ index.js
@@ -5,200 +5,140 @@
 //R_------------{  Update      :   05/01/2025   }-------------//
 
 //G_------------{  Start Hare  }------------
-
+let Letter1, Letter2, Letter3;
 function input1(){
-    let Letter1 = prompt("Please enter 1st last issue ");
+        Letter1 = prompt("Please enter 1st last issue ");
     let peraOutput1 = document.querySelector("#pera1");
         peraOutput1.innerHTML = ("1st issue is : " + Letter1);
 }
 function input2(){
-    let Letter2 = prompt("Please enter 2nd last issue ");
+        Letter2 = prompt("Please enter 2nd last issue ");
     let peraOutput2 = document.querySelector("#pera2");
         peraOutput2.innerHTML = ("2nd issue is : " + Letter2);
 }
 function input3(){
-    let Letter3 = prompt("Please enter 3rd last issue ");
+        Letter3 = prompt("Please enter 3rd last issue ");
     let peraOutput3 = document.querySelector("#pera3");
         peraOutput3.innerHTML = ("3rd issue is : " + Letter3);
 }
+// let subtraction = "Skip"
+function result(){
+    let finalResult = document.querySelector("#result-pera");
 
-
-
-
-
-
-
-// function output2(){
-//     let peraOutput1 = document.querySelector("#pera2");
-//     peraOutput1.innerHTML = Letter2;
-// }
-// function output3(){
-//     let peraOutput1 = document.querySelector("#pera3");
-//     peraOutput1.innerHTML = Letter3;
-// }
-
-
-
-
-
-if(Letter1===' ' || Letter2===' ' || Letter3===' ' ) {
-    console.log('Skip');
-}
-// R --- Letter1 = Big Single
-else if(Letter1 === 'Big Single' && Letter2 === 'Big Double' && Letter3 === 'Big Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
-    console.log('Add = Big Double, Small Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Small Double' && Letter3 === 'Big Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Small Double' && Letter3 === 'Small Double'){
-    console.log('Add = Big Single ');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
-    console.log('Add = Small Double ');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Small Single' && Letter3 === 'Big Double'){
-    console.log('Add = Small Double ');
-    console.log('Skip = ');
-}
-
-// R --- Letter1 = Small Single
-
-else if(Letter1 === 'Small Single' && Letter2 === 'Big Single' && Letter3 === 'Big Double'){
-    console.log('Add = Small Single ');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Single' && Letter2 === 'Big Double' && Letter3 === 'Big Double'){
-    console.log('Add = Small Double, Big Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Single' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Single' && Letter2 === 'Small Double' && Letter3 === 'Small Single'){
-    console.log('Add = Big Single');
-    console.log('Skip = ');
-}
-
-// R --- Letter1 = Big Double
-else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Small Double'){
-    console.log('Add = Big Single, Small Double, Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Big Single'){
-    console.log('Add = Big Double, Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Double' && Letter2 === 'Big Single' && Letter3 === 'Big Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
-    console.log('Add = Big Single');
-    console.log('Skip = ');
-}
-
-// R --- Letter1 = Small Double
-else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Big Double'){
-    console.log('Add = Big Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Small Single'){
-    console.log('Add = Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Big Double' && Letter3 === 'Small Single'){
-    console.log('Add = Small Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Small Double'){
-    console.log('Add = Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Big Double'){
-    console.log('Add = Small Double, Small Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Big Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
-    console.log('Add = Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Small Single'){
-    console.log('Add = Small Double, Big Double');
-    console.log('Skip = ');
+    // O ------------ Start Condition  ------------
+    if(Letter1==='' || Letter2==='' || Letter3==='' ) {
+            finalResult.innerHTML = ("Please Enter Correct thing" );
+    }
+    // G --------- Letter1 = Big Single   ---------
+    else if(Letter1 === 'Big Single' && Letter2 === 'Big Double' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Double, Small Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Small Double' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Small Double' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Small Single' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Small Double  <br> Skip = None");
+    }
+    // G --------- Letter1 = Small Single  ---------
+    else if(Letter1 === 'Small Single' && Letter2 === 'Big Single' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Small Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Single' && Letter2 === 'Big Double' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Small Double, Big Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Single' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Single' && Letter2 === 'Small Double' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Big Single <br> Skip = None");
+    }
+    // G --------- Letter1 = Big Double    ---------
+    else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Single, Small Double, Big Double <br> Skip = Small Double");
+    }
+    else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Big Double, Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Double' && Letter2 === 'Big Single' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Double' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Big Single <br> Skip = None");
+    }
+    // G --------- Letter1 = Small Double  ---------
+    else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Big Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Big Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Big Double' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Small Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Big Single' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Small Double, Small Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Single' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Small Double, Big Double <br> Skip = None");
+    }
+    // G --------- Letter1 = Big Single, Letter2 = Big Single      ---------
+    else if(Letter1 === 'Big Single' && Letter2 === 'Big Single' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Big Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Single' && Letter2 === 'Big Single' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    // G --------- Letter1 = Small Single, Letter2 = Small Single  ---------
+    else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Big Single'){
+        finalResult.innerHTML = ("Add = Small Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add =  Small Single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Small Single, Small Double <br> Skip = None");
+    }
+    // G --------- Letter1 = Big Double, Letter2 = Big Double      ---------
+    else if(Letter1 === 'Big Double' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
+        finalResult.innerHTML = ("Add = Small Single, Big Double <br> Skip = None");
+    }
+    else if(Letter1 === 'Big Double' && Letter2 === 'Big Double' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Big Double, Small Double, Small Single <br> Skip = Big Single");
+    }
+    // G --------- Letter1 = Small Double, Letter2 = Small Double  ---------
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
+        finalResult.innerHTML = ("Add = Big Double, Small single <br> Skip = None");
+    }
+    else if(Letter1 === 'Small Double' && Letter2 === 'Small Double' && Letter3 === 'Small Single'){
+        finalResult.innerHTML = ("Add = Big Double, Big Single <br> Skip = None");
+    }
+    // G --------- Letter1 || Letter2 || Letter are not add yet  ---------
+    else{
+            finalResult.innerHTML = ("Not Added Yet");
+    }
 }
 
-
-// R ------ Letter1 = Big Single, Letter2 = Big Single
-
-else if(Letter1 === 'Big Single' && Letter2 === 'Big Single' && Letter3 === 'Small Double'){
-    console.log('Add = Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Single' && Letter2 === 'Big Single' && Letter3 === 'Small Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-// R ------ Letter1 = Small Single, Letter2 = Small Single
-else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Big Single'){
-    console.log('Add = Small Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Small Double'){
-    console.log('Add = Small Single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Single' && Letter2 === 'Small Single' && Letter3 === 'Small Single'){
-    console.log('Add = Small Single, Small Double');
-    console.log('Skip = ');
-}
-
-
-// R ------ Letter1 = Big Double, Letter2 = Big Double
-else if(Letter1 === 'Big Double' && Letter2 === 'Big Double' && Letter3 === 'Small Double'){
-    console.log('Add = Small Single, Big Double');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Big Double' && Letter2 === 'Big Double' && Letter3 === 'Big Double'){
-    console.log('Add = Big Double, Small Double, Small Single');
-    console.log('Skip = ');
-}
-// R ------ Letter1 = Small Double, Letter2 = Small Double
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Double' && Letter3 === 'Big Double'){
-    console.log('Add = Big Double, Small single');
-    console.log('Skip = ');
-}
-else if(Letter1 === 'Small Double' && Letter2 === 'Small Double' && Letter3 === 'Small Single'){
-    console.log('Add = Big Double, Big Single');
-    console.log('Skip = ');
-}
-
-
-
-
-
-
-// Y -------else so add more condition
-else{
-    console.log('Not added yet');
-}
 // y             Big Single
 // y             Big Double
 // y             Small Single
